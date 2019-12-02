da5f82675e2701d0b927ec4c9ef924f00e7feb26
Juan Manuel Coyotzi Arellano <juan.coyotzi@inmersys.com>
2019-12-02 12:14:05 -0600

    Ormconfig changes

--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,10 @@
-{
+module.exports = {
     "type": "postgres",
-    "host": "localhost",
+    "host": process.env.TYPEORM_HOST,
     "port": 5432,
-    "username": "postgres",
-    "password": "WtSS6dpT2wwiu14f",
-    "database": "bioderma",
+    "username": process.env.TYPEORM_USERNAME,
+    "password": process.env.TYPEORM_PASSWORD,
+    "database": process.env.TYPEORM_DATABASE,
     "entities": [
         "src/core/**/**/**.entity{.ts,.js}"
     ],
