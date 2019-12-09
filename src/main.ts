697a09da7c995b92cf66710bf6a8b031934981db
Juan Manuel Coyotzi Arellano <juan.coyotzi@inmersys.com>
2019-12-09 17:47:03 -0600

    body parser fixes

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from './config/config.service';
-import * as bodyParser from 'body-parser';
+import { json } from 'body-parser';
 
 async function bootstrap() {
   const config = new ConfigService();
   const app = await NestFactory.create(AppModule);
-  app.use(bodyParser.json({ limit: '10mb' }));
-  app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
+  app.use(json({ limit: '50mb' }));
   app.enableCors();
   await app.listen(config.get('PORT'));
 }
