5004fe5b8c043fa1b06e14f661e309adc5ac60cc
Juan Manuel Coyotzi Arellano <juan.coyotzi@inmersys.com>
2020-02-18 16:21:27 -0600

    Submenu change before WebSockets integration

--- src/core/content/submenu/submenu.service.ts
+++ src/core/content/submenu/submenu.service.ts
@@ -51,6 +51,15 @@ export class SubmenuService {
             });
         }
 
+        if (menuId == 2) {
+            productList = await this.submenuRepository.find({
+                where: { menu: 1 },
+                order: {
+                    name: "ASC"
+                }
+            });
+        }
+
         const submenuList = await this.submenuRepository.find({
             where: { menu: menuId },
             order: {
@@ -60,6 +69,8 @@ export class SubmenuService {
 
         let listToReturn = [];
 
+        console.log("productList: ", productList);
+
         submenuList.forEach(tempFile => {
 
             let tempPDF = '';
@@ -67,25 +78,53 @@ export class SubmenuService {
             if (menuId == 1) {
                 switch (tempFile.id) {
                     case 2:
-                        tempPDF = productList[6].url;
-                        break;
-                    case 3:
                         tempPDF = productList[5].url;
                         break;
+                    case 3:
+                        tempPDF = productList[6].url;
+                        break;
                     case 4:
-                        tempPDF = productList[1].url;
+                        tempPDF = productList[2].url;
                         break;
                     case 5:
                         tempPDF = productList[4].url;
                         break;
                     case 6:
-                        tempPDF = productList[2].url;
+                        tempPDF = productList[3].url;
                         break;
                     case 7:
                         tempPDF = productList[0].url;
                         break;
                     case 8:
+                        tempPDF = productList[1].url;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            if (menuId == 2) {
+                switch (tempFile.id) {
+                    case 9:
+                        tempPDF = productList[1].url;
+                        break;
+                    case 10:
+                        tempPDF = productList[7].url;
+                        break;
+                    case 11:
+                        tempPDF = productList[5].url;
+                        break;
+                    case 12:
+                        tempPDF = productList[6].url;
+                        break;
+                    case 13:
                         tempPDF = productList[3].url;
+                        break;
+                    case 14:
+                        tempPDF = productList[2].url;
+                        break;
+                    case 15:
+                        tempPDF = productList[4].url;
                         break;
                     default:
                         break;
