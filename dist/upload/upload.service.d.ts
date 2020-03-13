3f4c6b192ce9dfbfc2e794c20cc0f2e95659266f
Juan Manuel Coyotzi Arellano <juan.coyotzi@inmersys.com>
2020-03-13 12:04:04 -0600

    multer fixes

--- dist/upload/upload.service.d.ts
+++ dist/upload/upload.service.d.ts
@@ -1,9 +1,10 @@
+/// <reference types="express" />
 export declare class UploadService {
     constructor();
     fileupload(req: any, res: any, folder: string): Promise<any>;
-    upload: any;
-    uploadProduct: any;
-    uploadCampaing: any;
-    uploadBlog: any;
-    uploadUser: any;
+    upload: import("express").RequestHandler<import("express-serve-static-core").ParamsDictionary>;
+    uploadProduct: import("express").RequestHandler<import("express-serve-static-core").ParamsDictionary>;
+    uploadCampaing: import("express").RequestHandler<import("express-serve-static-core").ParamsDictionary>;
+    uploadBlog: import("express").RequestHandler<import("express-serve-static-core").ParamsDictionary>;
+    uploadUser: import("express").RequestHandler<import("express-serve-static-core").ParamsDictionary>;
 }
