c45de1de8b59f8453239f73b2189c934e8e0f29a
Isaias Chavez <isaias.ch4vez@gmail.com>
2020-12-04 11:58:24 -0600

    .

--- ormconfig.js
+++ ormconfig.js
@@ -5,8 +5,8 @@ module.exports = {
     "username": process.env.TYPEORM_USERNAME,
     "password": process.env.TYPEORM_PASSWORD,
     "database": process.env.TYPEORM_DATABASE,
-    // "entities": [" dist/**/*.entity{.ts,.js} "],
-    "entities": ["src/**/*.entity{.ts,.js}"],
+    "entities": [" dist/**/*.entity{.ts,.js} "],
+    // "entities": ["src/**/*.entity{.ts,.js}"],
     // "migrations": ["src/database/migrations/*{.js}"],
     "migrations": ["dist/database/migrations/*{.js}"],
     "extra": {
